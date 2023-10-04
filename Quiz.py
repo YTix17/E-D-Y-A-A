243e814b22e0c8c11b7a3fec025ef04b2c427810
Yovani Tarazona <yovani2221881@correo.uis.edu.co>
2023-10-04 08:53:05 -0500

    Base
    No se que poner

---
+++ Quiz.py
@@ -0,0 +1,13 @@
+class Cliente:
+    def __init__(self, nombre, cedula, habitacion):
+        self.nombre = nombre
+        self.cedula = cedula
+        self.habitacion = habitacion
+        
+    def consulta(self):
+        pass
+    
+    def consulta_habitaciones(self):
+        pass
+
+cliente = Cliente()
